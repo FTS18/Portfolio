d274d73c29ae473f9d3a4eee14a4b31d185e85f0
FTS18 <anandsagardubey71568@gmail.com>
2023-04-18 22:36:50 +0530

    Optimzed images for faster reload

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,3 @@ burger.addEventListener('click', function() {
     burger.classList.toggle('open');
     body.classList.toggle('disabledScroll');
 });
-const container = document.querySelector(".slideshow-container");
-const text = document.querySelector(".title");
-const image = new Image();
-
-image.src = container.style.backgroundImage.replace(/url\((['"])?(.*?)\1\)/gi, '$2');
-image.crossOrigin = "Anonymous";
-
-image.addEventListener("load", function() {
-    const colorThief = new ColorThief();
-    const color = colorThief.getColor(image);
-    const textColor = getContrastYIQ(color);
-    text.style.color = textColor;
-});
-
-function getContrastYIQ(backgroundColor) {
-    const yiq = ((backgroundColor[0] * 299) + (backgroundColor[1] * 587) + (backgroundColor[2] * 114)) / 1000;
-    return (yiq >= 128) ? '#000000' : '#e7e7e7e7e7e7';
-}
