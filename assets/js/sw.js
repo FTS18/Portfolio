058ee2a8c75b7ee6bf059f3593db511cd24a5f86
FTS18 <anandsagardubey71568@gmail.com>
2023-04-19 02:47:41 +0530

    Increased Accessibility

---
+++ assets/js/sw.js
@@ -0,0 +1,53 @@
+const cacheName = "cache-v1";
+const ContentToCache = [
+    "/js13kpwa/",
+    "/index.html",
+    "assets/js/script.js",
+    "assets/css/style.css",
+    "assets/css/theme.css",
+    "components/header.js",
+];
+
+// contentToCache.push("");
+
+self.addEventListener("install", (e) => {
+    console.log("[Service Worker] Install");
+    e.waitUntil(
+        (async() => {
+            const cache = await caches.open(cacheName);
+            console.log("[Service Worker] Caching all: app shell and content");
+            await cache.addAll(ContentToCache);
+        })()
+    );
+});
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async() => {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) {
+                return r;
+            }
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })()
+    );
+});
+
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map((key) => {
+                    if (key === cacheName) {
+                        return;
+                    }
+                    return caches.delete(key);
+                })
+            );
+        })
+    );
+});
