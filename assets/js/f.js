5173722c9e0ebfd6fcea842e068db5534dd1e87c
FTS18 <anandsagardubey71568@gmail.com>
2023-06-02 18:38:03 +0530

    Updated

---
+++ assets/js/f.js
@@ -0,0 +1,12 @@
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js')
+            .then(registration => {
+                console.log('Service Worker is registered with scope: ', registration.scope);
+            })
+            .catch(err => {
+                console.error('Registration failed: ', err);
+            });
+    });
+};
--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import http from 'http';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path from 'path';
-
+//AIzaSyAe1zGORzeLjOLOXWAnZVu74k1xxoLw5Nk
 const app = express();
 const router = express.Router();
 app.use(cors());
@@ -42,21 +42,7 @@ router.get('/download', async(req, res) => {
         console.error('Error saving the video:', err);
     }
 });
-app.use('/api', router);
-
-const __filename = fileURLToPath(
-    import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const socketPath = path.resolve(__dirname, 'socket.sock'); // Replace with your desired socket file path
-
-// Create the socket file
-if (fs.existsSync(socketPath)) {
-    fs.unlinkSync(socketPath);
-}
-
-const server = http.createServer(app);
-server.listen(socketPath, () => {
-    fs.chmodSync(socketPath, '777');
-    console.log(`Server is running on Unix socket ${socketPath}`);
+// Start the server
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 });
